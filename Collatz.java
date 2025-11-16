816ab41d4fafff12407e848743c079fa9d08a5d8
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2025-11-16 15:12:12 +0200

    Feedback Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,3 +1,12 @@
+/*
+Feedback:
+Great work!
+Yam
+*/
+
+
+
+
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
@@ -35,3 +44,4 @@ public class Collatz {
 	System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 	}
 }
+
